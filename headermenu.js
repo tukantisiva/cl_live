9629d5a914b1e0c10fa689e511c959d03655687c
yamunaph <57438179+yamunaph@users.noreply.github.com>
2019-11-10 16:13:40 +0400

    headermenu.jsfile added

---
+++ headermenu.js
@@ -0,0 +1,6 @@
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+   $(document).ready(function()
+		     {
+      $("#headermenu").load("headermenu.html"); 
+    });
+    </script>
