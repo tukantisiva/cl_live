f7f4b40d8eaf50b8da5e0d6cc7d0984fd35ec378
yamunaph <57438179+yamunaph@users.noreply.github.com>
2019-11-10 16:17:43 +0400

    Update headermenu.js

--- js/headermenu.js
+++ js/headermenu.js
@@ -1,4 +1,5 @@
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
    $(document).ready(function()
 		     {
       $("#headermenu").load("headermenu.html"); 
