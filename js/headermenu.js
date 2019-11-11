c025eff95c0653b38251230575d58f28e1f7c51f
yamunaph <57438179+yamunaph@users.noreply.github.com>
2019-11-11 10:54:49 +0400

    Update headermenu.js

--- js/headermenu.js
+++ js/headermenu.js
@@ -1,5 +1,233 @@
-$( window ).load(function() {
+var htmlvar= '<header class="header-three">';
+
+ htmlvar+='<!--Logo Mainmenu Start-->';
+
+       htmlvar+='<div class="header-logo-menu sticker">';
+
+           htmlvar+='<div class="container-fluid">';
+
+            htmlvar+='<div class="logo-menu-bg">';
+
+              htmlvar+='<div class="row">';
+
+                        htmlvar+='<div class="col-lg-3 col-md-12">';
+
+                            htmlvar+='<div class="logo">';
+
+                                htmlvar+='<a href="index.html"><img src="img\logo\logo.png" alt="commitlearners"></a>';
+
+                            htmlvar+='</div>';
+
+                        htmlvar+='</div>';
+
+
+
+                        htmlvar+='<div class="col-lg-9 d-none d-lg-block">';
+
+                            htmlvar+='<div class="mainmenu-area">';
+
+                                htmlvar+='<div class="mainmenu">';
+
+                                    htmlvar+='<nav>';
+
+                                        htmlvar+='<ul id="nav">';
+
+                                            htmlvar+='<li class="current"><a href="contact.html"><button class="btn btn-info">Request Free Demo</button></a></li>';
+
+                                            htmlvar+='<li class="current"><a href="index.html">Home</a></li>';
+
+
+
+                                            htmlvar+='<li><a href="about.html">About us</a></li>';
+
+
+
+                                            htmlvar+='<li><a href="studentreg.html">Register<i class="fa fa-angle-down"></i></a>';
+
+                                                htmlvar+='<ul class="sub-menu">';
+
+                                                    htmlvar+='<li><a href="studentreg.html">Student</a></li>';
+
+                                                    htmlvar+='<li><a href="tutorreg.html">Tutor</a></li>';
+
+                                                htmlvar+='</ul>';
+
+                                            htmlvar+='</li>';
+
+                                           htmlvar+=' <li><a href="#">Tuition<i class="fa fa-angle-down"></i></a>';
+
+                                                htmlvar+='<ul class="sub-menu">';
+
+                                                    htmlvar+='<li><a href="studentreg.html">School</a></li>';
+
+                                                    htmlvar+='<li><a href="studentreg.html">College</a></li>';
+
+                                                    htmlvar+='<li><a href="studentreg.html">University</a></li>';
+
+                                                htmlvar+='</ul>';
+
+                                            htmlvar+='</li>';
+
+                                            htmlvar+='<li><a href="fullstack.html">IT Courses<i class="fa fa-angle-down"></i></a>';
+
+                                                htmlvar+='<ul class="sub-menu">';
+
+                                                    htmlvar+='<li><a href="fullstack.html">Full Stack</a></li>';
+
+                                                htmlvar+='</ul>';
+
+                                            htmlvar+='</li>';
+					 htmlvar+='<li><a href="Online%20maths%20subject%20learning.html">Blogs<i class="fa fa-angle-down"></i></a>';
+                                            htmlvar+='<ul class="sub-menu">';
+                                               htmlvar+='<li><a href="top%205%20online%20education%20tuitions%20benefits.html">Benefits of Online Classes</a></li>';
+						htmlvar+='<li><a href="Online%20maths%20subject%20learning.html">Benefits of Online Maths Classes</a></li>';
+						htmlvar+='<li><a href="AboutOnlineFullStackCourse.html">About Online Fullstack Classes</a></li>';
+                                           htmlvar+='</ul>';
+                                        htmlvar+='</li>';
+
+                                            htmlvar+='<li><a href="findjob.html">Find Jobs</a></li>';
+
+                                            htmlvar+='<li><a href="formpage.html">Contact Us</a></li>';
+
+
+                                        htmlvar+='</ul>';
+
+                                    htmlvar+='</nav>';
+
+                                htmlvar+='</div>';
+
+                                htmlvar+='<!-- <ul class="header-search">';
+
+                                        htmlvar+='<li class="btn btn-info">';
+
+                                            htmlvar+='<i id="toggle-search" class=""></i>';
+
+                                        htmlvar+='</li>';
+
+                                    htmlvar+='</ul> -->';
+
+                                htmlvar+='<!--Search Form-->';
+
+                                htmlvar+='<!-- <div class="search">';
+
+                                       htmlvar+=' <div class="search-form">';
+
+                                            htmlvar+='<form id="search-form" action="#">';
+
+                                                htmlvar+='<input type="search" placeholder="Search here..." name="search" />';
+
+                                                htmlvar+='<button type="submit">';
+
+                                                    htmlvar+='<span><i class="fa fa-search"></i></span>';
+
+                                                htmlvar+='</button>';
+
+                                            htmlvar+='</form>';                             
+
+                                        htmlvar+='</div>';
+
+                                    htmlvar+='</div> -->';
+
+                                htmlvar+='<!--End of Search Form-->';
+
+                            htmlvar+='</div>';
+
+                        htmlvar+='</div>';
+
+                    htmlvar+='</div>';
+
+                htmlvar+='</div>';
+
+            htmlvar+='</div>';
+
+        htmlvar+='</div>';
+
+        htmlvar+='<!--End of Logo Mainmenu-->';
+
+        htmlvar+='<!-- Mobile Menu Area start -->';
+
+        htmlvar+='<div class="mobile-menu-area">';
+
+            htmlvar+='<div class="container">';
+
+                htmlvar+='<div class="row">';
+
+                    htmlvar+='<div class="col-lg-12 col-md-12 col-sm-12">';
+
+                        htmlvar+='<div class="mobile-menu">';
+
+                            htmlvar+='<nav id="dropdown">';
+
+                                htmlvar+='<ul>';
+
+                                    htmlvar+='<li><a href="index.html">Home</a></li>';
+				    htmlvar+='<li><a href="about.html">About Us</a></li>';
+					htmlvar+='<li><a href="studentreg.html">Register</a>';
+
+                                        htmlvar+='<ul>';
+
+                                            htmlvar+='<li><a href="studentreg.html">Student</a></li>';
+
+                                            htmlvar+='<li><a href="tutorreg.html">Tutor</a></li>';
+
+                                        htmlvar+='</ul>';
+
+                                    htmlvar+='</li>';
+                                    htmlvar+='<li><a href="fullstack.html">Tuition</a>';
+
+                                        htmlvar+='<ul>';
+
+                                            htmlvar+='<li><a href="#">School</a></li>';
+
+                                            htmlvar+='<li><a href="#">College</a></li>';
+
+                                            htmlvar+='<li><a href="#">University</a></li>';
+
+                                        htmlvar+='</ul>';
+
+                                    htmlvar+='</li>';
+
+                                    htmlvar+='<li><a href="fullstack.html">IT Courses</a>';
+
+                                        htmlvar+='<ul>';
+
+                                            htmlvar+='<li><a href="fullstack.html">Full Stack</a></li>';
+
+                                       htmlvar+='</ul>';
+
+                                    htmlvar+='</li>';
+				  htmlvar+='<li><a href="Online%20maths%20subject%20learning.html">Blogs</a>';
+                                            htmlvar+='<ul class="sub-menu">';
+                                               htmlvar+='<li><a href="top%205%20online%20education%20tuitions%20benefits.html">Benefits of Online Classes</a></li>';
+						htmlvar+='<li><a href="Online%20maths%20subject%20learning.html">Benefits of Online Maths Classes</a></li>';
+						htmlvar+='<li><a href="AboutOnlineFullStackCourse.html">About Online Fullstack Classes</a></li>';
+                                           htmlvar+='</ul>';
+                                        htmlvar+='</li>';
+                                   htmlvar+=' <li><a href="#">Find Jobs</a></li>';
+
+                                    htmlvar+='<li><a href="contact.html">Contact</a></li>';
+
+                                htmlvar+='</ul>';
+
+                            htmlvar+='</nav>';
+
+                        htmlvar+='</div>';
+
+                    htmlvar+='</div>';
+
+                htmlvar+='</div>';
+
+            htmlvar+='</div>';
+
+        htmlvar+='</div>';
+
+        htmlvar+='<!-- Mobile Menu Area end -->';
+
+    htmlvar+='</header>';
+
+    htmlvar+='<!--End of Header Area-->';
+
 		     
-      $("#headermenu").load("headermenu.html"); 
-    });
+      $("#headermenu").html(htmlvar);
+
 
