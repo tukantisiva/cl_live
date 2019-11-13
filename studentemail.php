56554612e4995ca10733f7ac0464766539726255
yamunaph <57438179+yamunaph@users.noreply.github.com>
2019-11-13 10:51:43 +0400

    studentemail php file added

---
+++ studentemail.php
@@ -0,0 +1,4 @@
+<?php
+    mail('contact@commitleaners.com', $_POST['name'],$_POST['pname'],$_POST['email'],$_POST['mobile'],$_POST['mobile'],$_POST['cmun'],$_POST['country'],$_POST['tuition'],$_POST['classtype'], $_POST['subjects'],$_POST['description']);
+?>
+<p>Thanks for posting your details with us.We will contact you soon.</p>
