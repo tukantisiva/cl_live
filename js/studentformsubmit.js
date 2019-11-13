c741bcfe4e4c02ac3e1b04b79d3a31ab7cd81142
yamunaph <57438179+yamunaph@users.noreply.github.com>
2019-11-13 11:43:11 +0400

    added studentform submit js

---
+++ js/studentformsubmit.js
@@ -0,0 +1,65 @@
+
+$(function()
+{
+	$('#bodyofstudent').show();
+    function after_form_submitted(data) 
+    {
+        if(data.result == 'success')
+        {
+            $('form#studentform').hide();
+            $('#success_message').show();
+		$('#bodyofstudent').hide();
+            $('#error_message').hide();
+        }
+        else
+        {
+            $('#error_message').append('<ul></ul>');
+
+            jQuery.each(data.errors,function(key,val)
+            {
+                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
+            });
+            $('#success_message').hide();
+            $('#error_message').show();
+
+            //reverse the response on the button
+            $('button[type="button"]', $form).each(function()
+            {
+                $btn = $(this);
+                label = $btn.prop('orig_label');
+                if(label)
+                {
+                    $btn.prop('type','submit' ); 
+                    $btn.text(label);
+                    $btn.prop('orig_label','');
+                }
+            });
+            
+        }//else
+    }
+
+	$('#studentform').submit(function(e)
+      {
+        e.preventDefault();
+
+        $form = $(this);
+        //show some response on the button
+        $('button[type="submit"]', $form).each(function()
+        {
+            $btn = $(this);
+            $btn.prop('type','button' ); 
+            $btn.prop('orig_label',$btn.text());
+            $btn.text('Sending ...');
+        });
+        
+
+                    $.ajax({
+                type: "POST",
+                url: 'handler.php',
+                data: $form.serialize(),
+                success: after_form_submitted,
+                dataType: 'json' 
+            });        
+        
+      });	
+});
